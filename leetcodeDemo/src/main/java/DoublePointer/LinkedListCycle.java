03cebd4876b644df578891a52f54f6e250064324
Potato <516768101@qq.com>
2020-11-13 23:38:02 +0800

    142. 环形链表 II

---
+++ leetcodeDemo/src/main/java/DoublePointer/LinkedListCycle.java
@@ -0,0 +1,46 @@
+package DoublePointer;
+
+/**
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ *
+ 输入：head = [3,2,0,-4], pos = 1
+ 输出：返回索引为 1 的链表节点
+ 解释：链表中有一个环，其尾部连接到第二个节点。
+ */
+
+/**
+ * 思路：链表找环路，一般都是使用双指针绕圈法来解决
+ */
+public class LinkedListCycle {
+    public static void main(String[] args) {
+        ListNode head = null;
+        ListNode fast = head, slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) {
+//                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+//        return fast;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
