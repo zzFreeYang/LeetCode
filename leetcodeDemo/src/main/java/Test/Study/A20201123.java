a5137beab7c9c94cf5bac3e13101e8138d4d33c1
Potato <516768101@qq.com>
2020-11-25 00:21:37 +0800

    todo

--- leetcodeDemo/src/main/java/Test/Study/A20201123.java
+++ leetcodeDemo/src/main/java/Test/Study/A20201123.java
@@ -1,7 +1,10 @@
 package Test.Study;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class A20201123 {
     public int sharedState;
+    ReentrantLock reentrantLock = new ReentrantLock(true);
 
     /**
      *   非安全操作:
@@ -31,25 +34,45 @@ public class A20201123 {
          }
         }
     }
+
+    /**
+     *   安全操作:
+     *      ReentrantLock
+     */
+    public void SafeActionByReentrantLock() {
+        while (sharedState < 100000) {
+            try{
+                reentrantLock.lock();  //可重入的测试
+                reentrantLock.lock();
+                int former = sharedState++;
+                int latter = sharedState;
+                if (former != latter - 1) {
+                    System.out.printf("Observed data race, former is " + former + ", " + "latter is " + latter);
+                }
+            }finally {
+                reentrantLock.unlock();
+                reentrantLock.unlock();
+            }
+        }
+    }
     public static void main (String[]args) throws InterruptedException {
         final A20201123 sample = new A20201123();
         Thread threadA = new Thread(){
             @Override
             public void run(){
-                sample.SafeAction();
+                sample.SafeActionByReentrantLock();
             }
         };
         Thread threadB = new Thread(){
             @Override
             public void run() {
-                sample.SafeAction();
+                sample.SafeActionByReentrantLock();
             }
         };
         threadA.start();
         threadB.start();
         threadA.join();
         threadB.join(); }
-
 }
 
 
