4f6d48ace2c48ec9c17674c7b03ebd939c0e07d2
Potato <516768101@qq.com>
2020-11-25 23:10:57 +0800

    一个线程两次调用start()方法会出现什么情况？

---
+++ leetcodeDemo/src/main/java/Test/Study/A20201125.java
@@ -0,0 +1,17 @@
+package Test.Study;
+
+public class A20201125 {
+    /**
+     * 线程实现的两个基本方法: 1. 扩展 Thread类 2 .实现 Runnable
+     * @param args
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            System.out.println("Hello World!");
+        };
+        Thread myThread = new Thread(task);
+        myThread.start();
+        myThread.join();  //由于 myThread.join了，所以end一定在hello world后面
+        System.out.println("end");
+    }
+}
