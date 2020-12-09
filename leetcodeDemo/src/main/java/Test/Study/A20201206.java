f568613e52aa50afb5a862a573fa7694ed7bd891
Potato <516768101@qq.com>
2020-12-09 21:38:34 +0800

    Java并发类库提供的线程池有哪几种？

---
+++ leetcodeDemo/src/main/java/Test/Study/A20201206.java
@@ -0,0 +1,15 @@
+package Test.Study;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.util.concurrent.Executors.*;
+
+public class A20201206 {
+    public static void main(String[] args) {
+        ExecutorService executorService1 = newSingleThreadExecutor();
+        ExecutorService executorService2 = newFixedThreadPool(4);
+        ExecutorService executorService3 = newCachedThreadPool();
+    }
+}
