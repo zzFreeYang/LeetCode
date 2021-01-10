68b65467265f39135832748088e9043c65843834
Potato <516768101@qq.com>
2021-01-10 23:44:57 +0800

    2021.01.10

---
+++ leetcodeDemo/src/main/java/OneHundredTops/Subsets.java
@@ -0,0 +1,19 @@
+package OneHundredTops;
+
+//       回溯
+
+/**
+ * 给你一个整数数组 nums ，返回该数组所有可能的子集（幂集）。解集不能包含重复的子集。
+ *
+ *输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ */
+public class Subsets {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
