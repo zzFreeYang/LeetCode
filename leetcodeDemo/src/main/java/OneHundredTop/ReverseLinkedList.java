a528e397981a0e8f91c1943c3b238c99c59ed5f1
Potato <516768101@qq.com>
2021-03-02 23:12:27 +0800

    反转链表

---
+++ leetcodeDemo/src/main/java/OneHundredTop/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package OneHundredTop;
+
+/**
+ *  反转链表
+ *  经典解法
+ */
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+//        public ListNode reverseList(ListNode head) {
+//            ListNode pre = null;
+//            ListNode cur = head;
+//            while(cur!=null){
+//                ListNode temp = cur.next;
+//                cur.next = pre;
+//                pre = cur;
+//                cur = temp;
+//
+//            }
+//            return pre;
+//        }
+    }
+
+
+
+}
